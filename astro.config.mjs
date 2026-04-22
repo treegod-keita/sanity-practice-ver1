522b3361fbb8bdfdc0260b31286e17ec42158709
榊啓太 <treegod0205@gmail.com>
2026-04-23 00:34:39 +0900

    fix: envファイルの呼び出し方法変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sanity from "@sanity/astro";
-import { loadEnv } from "vite";
+import dotenv from "dotenv";
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+dotenv.config();
 
-// https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
     integrations: [
-        // 👇 update these lines
         sanity({
-            projectId: env.PUBLIC_SANITY_PROJECT_ID,
-            dataset: env.PUBLIC_SANITY_DATASET,
+            projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
+            dataset: process.env.PUBLIC_SANITY_DATASET,
             useCdn: false,
             apiVersion: "2026-04-18",
         }),
